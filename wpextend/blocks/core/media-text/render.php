7082e56fafd3503435a5eaa1622898fec92e6791
paul.balanche <pb@buzzbrothers.ch>
2020-05-28 12:09:54 +0200

    Update media-text block rendering

--- wpextend/blocks/core/media-text/render.php
+++ wpextend/blocks/core/media-text/render.php
@@ -2,15 +2,48 @@
 
 function core_media_text_render_callback( $attributes, $content ) {
 
-    echo htmlentities($content);
-    echo '<pre>';print_r($attributes);die;
+    // $attributes['isStackedOnMobile']
+    // $attributes['verticalAlignment']
+    // $attributes['imageFill']
 
+    if( isset($attributes['mediaId'], $attributes['mediaType']) ) {
 
-    preg_match( '/^<figure[^>]*">(.*)<\/figure>$/ms', $content, $matches );
-    if( count($matches) == 2 ) {
+        if( $attributes['mediaType'] == 'image' ) {
+            
+            $data_image = wp_get_attachment_image_src($attributes['mediaId'], 'large');
+            $html_media = '<img src="' . $data_image[0] . '" alt="" />';
+        }
+        else if( $attributes['mediaType'] == 'video' ) {
 
-        return '<div class="container">
-            <div class="row">' . $matches[1] . '</div>
-        </div>';
+            preg_match( '/<figure class="wp-block-media-text__media">(.*)<\/figure>/ms', $content, $matches );
+            if( count($matches) == 2 ) {
+
+                $html_media = $matches[1];
+            }
+        }
+
+        $mediaPosition = ( isset($attributes['mediaPosition']) ) ? $attributes['mediaPosition'] : 'left';
+
+        preg_match( '/<div class="wp-block-media-text__content">(.*)<\/div><\/div>/ms', $content, $matches );
+        if( count($matches) == 2 ) {
+            
+            $return = '<div class="container">
+                <div class="row">';
+
+            if( $mediaPosition == 'left' ) {
+
+                $return .= '<div class="col">' . $html_media . '</div>
+                    <div class="col">' . $matches[1] . '</div>';
+            }
+            else {
+                $return .= '<div class="col">' . $matches[1] . '</div>
+                    <div class="col">' . $html_media . '</div>';
+            }
+
+            $return .= '</div>
+            </div>';
+
+            return $return;
+        }
     }
 }

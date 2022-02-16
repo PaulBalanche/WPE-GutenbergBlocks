75cddff45944877a17d093275fbb4c54a8d39911
Paul Balanche <pb@buzzbrothers.ch>
2022-02-16 09:21:50 +0100

    Update wpe-component image render

--- blocks/custom/wpe-component/render.php
+++ blocks/custom/wpe-component/render.php
@@ -111,21 +111,35 @@ function custom_wpe_component_attributes_formatting($component, $attributes) {
                         $images = ( $prop['repeatable'] ) ? $attributes[$key_prop] : [ $attributes[$key_prop] ];
                         foreach( $images as $key_image => $current_image ) {
 
-                            if( is_array($current_image) && isset($current_image['id']) ) {
-                                $attachment_image_src = wp_get_attachment_image_src($current_image['id'], 'large');
-                                if( is_array($attachment_image_src) ) {
+                            if( is_array($current_image) ) {
 
-                                    $current_image['src'] = $attachment_image_src[0];
-                                    $current_image['url'] = $attachment_image_src[0];
-                                    $current_image['alt'] = trim( strip_tags( get_post_meta( $current_image['id'], '_wp_attachment_image_alt', true ) ) );
+                                foreach( $current_image as $responsive_key => $responsive_image ) {
 
-                                    unset($current_image['id']);
-                                    unset($current_image['preview']);
-            
-                                    if( isset($prop['root_prop']) && isset( $current_image[ $prop['root_prop'] ] ) )
-                                        $images[$key_image] = $current_image[ $prop['root_prop'] ];
+                                    if( is_array($responsive_image) && isset($responsive_image['id']) ) {
+
+                                        $image_size = ( isset($prop['image_size_identifier']) && is_array($prop['image_size_identifier']) && isset($prop['image_size_identifier'][$responsive_key]) ) ? $prop['image_size_identifier'][$responsive_key] : 'large';
+                                        $attachment_image_src = wp_get_attachment_image_src($responsive_image['id'], $image_size);
+                                        if( is_array($attachment_image_src) ) {
+
+                                            $responsive_image['src'] = $attachment_image_src[0];
+                                            $responsive_image['url'] = $attachment_image_src[0];
+                                            $responsive_image['alt'] = trim( strip_tags( get_post_meta( $responsive_image['id'], '_wp_attachment_image_alt', true ) ) );
+
+                                            unset($responsive_image['id']);
+                                            unset($responsive_image['preview']);
+                                        }
+                                    }
                                     else
-                                        $images[$key_image] = (object) $current_image;
+                                        $responsive_image = null;
+
+                                    if( ! is_null($responsive_image) ) {
+                                        if( isset($prop['root_prop']) && isset( $responsive_image[ $prop['root_prop'] ] ) )
+                                            $images[$key_image][$responsive_key] = $responsive_image[ $prop['root_prop'] ];
+                                        else
+                                            $images[$key_image][$responsive_key] = (object) $responsive_image;
+                                    }
+                                    else
+                                        unset( $images[$key_image][$responsive_key] );
                                 }
                             }
                         }
